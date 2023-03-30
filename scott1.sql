5183c638a32c02b546e5861daae390fef2f2de63
909ma <sin00021@naver.com>
2023-03-30 11:01:19 +0900

    user table

---
+++ scott1.sql
@@ -0,0 +1,13 @@
+create table users
+(
+user_id varchar2(100) primary key,
+user_email varchar2(100) not null unique,
+user_balance double default 0,
+user_country varchar2(100),
+user_city varchar2(100),   -- 시(ex 대구)
+user_street varchar2(100), -- 도로명 주소
+user_registDate date,
+user_birthDate date,
+user_name varchar2(100) not null
+);
+
