5bae9953ff0012265f3c38e4bc5cb0aec3fc8e05
909ma <sin00021@naver.com>
2023-04-03 16:06:24 +0900

    users,country,city 분류

--- scott1.sql
+++ scott1.sql
@@ -1,13 +1,25 @@
+create table country
+(
+country_code varchar2(10) primary key,
+country_name_kr varchar2(100) not null
+);
+
+create table city
+(
+city_code varchar2(10) primary key,
+city_name_kr varchar2(100) not null,
+city_country_code varchar2(10) references country(country_code)
+);
+
 create table users
 (
 user_id varchar2(100) primary key,
 user_email varchar2(100) not null unique,
 user_balance number(38,2) default 0,
-user_country varchar2(100),
-user_city varchar2(100),   -- 시(ex 대구)
-user_street varchar2(100), -- 도로명 주소
+user_country varchar2(10) references country(country_code),
+user_city varchar2(10) references city(city_code),   
+user_street varchar2(100), 
 user_registDate date,
 user_birthDate date,
 user_name varchar2(100) not null
 );
-
